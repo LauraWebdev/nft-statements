a68a497095ff239e80ea9083b178ef380264b3ff
Laura Heimann <hello@laura.media>
2022-01-01 14:01:03 +0100

    fixed correct branch in deployment

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -18,9 +18,9 @@ import fs from 'fs';
         console.log('Pushing to gh-pages...');
         await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
         await execa('rm', ['-r', folderName]);
-        await execa('git', ['checkout', '-f', 'master']);
+        await execa('git', ['checkout', '-f', 'main']);
         await execa('git', ['branch', '-D', 'gh-pages']);
-        
+
         console.log('Successfully deployed, check your settings');
     } catch (e) {
         // eslint-disable-next-line no-console
