b1f346c63d902cbfd9ca3cd9e39c16919035b90e
Laura Heimann <hello@laura.media>
2022-01-01 13:12:28 +0100

    Updated linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    'plugin:vue/recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  'plugins': [
-    'vue'
-  ],
-  'rules': {
-    'indent': ['error', 4],
-    'vue/html-indent': ['error', 4],
-    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-    'semi': [2, 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        'plugin:vue/recommended',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    'plugins': [
+        'vue',
+    ],
+    'rules': {
+        'indent': ['error', 4],
+        'vue/html-indent': ['error', 4],
+        'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+        'semi': [2, 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'vue/require-default-prop': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+};
